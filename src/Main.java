cbe3c53be3495de18e7a349c61f3a6c38f7f43ba
Yuriy <yura.qa53@gmail.com>
2023-01-07 20:31:23 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         int valueTicket = 1000;
         int bonusRubles = 20;
 
-        System.out.println("Количество начисленных миль, за купленный билет:");
+        System.out.println("Количество начисленных миль за купленный билет:");
         System.out.println((valueTicket / bonusRubles));
 
     }
